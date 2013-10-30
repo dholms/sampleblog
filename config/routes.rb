ad6f5cab2317d32f87be8fd98323a22f139a6519
dholms <dansdebate@gmail.com>
2013-10-30 16:44:48 -0400

    Added the ability to comment. Includes comment model and controller.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Sampleblog::Application.routes.draw do
   # You can have the root of your site routed with "root"
    root to: 'welcome#index'
 
+  resources :posts do
+    resources :comments
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
