4fe8a56909bc58bfcdcf5c27619012e15e350d50
dholms <dansdebate@gmail.com>
2013-11-03 22:09:54 -0500

    fixed edit modal and changed homepage

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Sampleblog::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root to: 'welcome#index'
+   root to: 'posts#index'
 
   resources :posts do
     resources :comments
